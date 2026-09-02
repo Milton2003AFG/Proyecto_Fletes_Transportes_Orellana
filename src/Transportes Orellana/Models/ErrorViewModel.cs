cc04590a375a1047c0c0363669a9cfdaf1915516
Milton2003AFG <miltonflores2003@gmail.com>
2026-09-02 14:22:51 -0600

    chore: Estructura inicial de proyecto

---
+++ src/Transportes Orellana/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace Transportes_Orellana.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
